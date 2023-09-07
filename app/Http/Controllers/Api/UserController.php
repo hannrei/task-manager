acb841130100833461cd15cd3e19bd56cf875748
hannrei <130257338+hannrei@users.noreply.github.com>
2023-09-07 15:46:58 +0200

    User Controller and Seeder

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class USerController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+        $this->authorizeResource(User::class, 'user');
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        User::paginate(100);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        auth()->user();
+
+        return $user;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, User $user)
+    {
+        $user = auth()->user();
+
+        $request->validate([
+            'name' => 'sometimes|string|max:255',
+            'email' => 'sometimes|email',
+            'password' => 'sometimes|string|min:8|confirmed',
+        ]);
+
+        if (User::where('email', $request->email)->exists()) {
+            return response()->json([
+                'message' => 'The given data was invalid.',
+            ], 422);
+        }
+
+        $user->update([
+            'name' => $request->name ?? $user->name,
+            'email' => $request->email ?? $user->email,
+            'password' => $request->password ?? $user->password,
+        ]);
+
+        return response()->json(
+            [
+                'message' => 'User updated successfully.',
+                'user' => $user,
+            ],
+            200
+        );
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        $user = auth()->user();
+
+        $user->delete();
+
+        return response()->json([
+            'message' => 'User deleted successfully.',
+        ], 200);
+    }
+}
--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -17,11 +17,11 @@ class UsersTableSeeder extends Seeder
         $adminRole = Role::where('name', 'admin')->first();
         $userRole = Role::where('name', 'user')->first();
 
-        User::factory(1000)->create()->each(function (User $user) use ($userRole, $adminRole) {
+        User::factory(100)->create()->each(function (User $user) use ($userRole, $adminRole) {
 
             $user->roles()->attach($userRole->id);
 
-            if ($user->id % 2 === 0) {
+            if (rand(1, 2) % 2 === 0) {
                 $user->roles()->attach($adminRole->id);
             }
         });
