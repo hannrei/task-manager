bbd8565d708569d8e83dc9776a1181a4301b7833
hannrei <130257338+hannrei@users.noreply.github.com>
2023-09-13 22:29:12 +0200

    Short doc for the project.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,33 +6,10 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(title="Task Manager API", version="0.1")
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
-
-    /**
-     * @OA\Info(
-     *      version="1.0.0",
-     *      title="Task Manager API Documentation",
-     *      description="Task Manager API Documentation. This is a sample API for Task Manager.",
-     *      @OA\Contact(
-     *          email="demo@email.com"
-     *      ),
-     *      @OA\License(
-     *          name="",
-     *          url=""
-     *      )
-     * )
-     *
-     * @OA\Server(
-     *      url=L5_SWAGGER_CONST_HOST,
-     *      description="Task Manager API Server"
-     * )
-
-     *
-     * @OA\Tag(
-     *     name="Task Manager",
-     *     description="API Endpoints of Task Manager"
-     * )
-     */
 }
