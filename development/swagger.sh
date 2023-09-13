801f30094a3a1086479198e8ee0d6e7fa6c56d87
hannrei <130257338+hannrei@users.noreply.github.com>
2023-09-13 21:09:12 +0200

    Fixed swagger.sh script.

--- development/swagger.sh
+++ development/swagger.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-mkdir ../public/swagger
-php ../vendor/bin/swagger --bootstrap ./swagger-constants.php --output ../public/swagger ./swagger-v1.php ../app/Http/Controllers
+# Output directory
+directory="../public/swagger"
+
+# Check if the directory exists
+if [ ! -d "$directory" ]; then
+    # Directory doesn't exist, create it
+    mkdir -p "$directory"
+    echo "Directory '$directory' created."
+else
+    # Directory already exists
+    echo "Directory '$directory' already exists."
+fi
+
+php ../vendor/zircote/swagger-php/bin/openapi --bootstrap ./swagger-constants.php --output ../public/swagger ./swagger-v1.php ../app/Http/Controllers
