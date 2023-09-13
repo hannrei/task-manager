4b18406593505e381a8fb98375ed0d0b297a9854
hannrei <130257338+hannrei@users.noreply.github.com>
2023-09-13 23:09:16 +0200

    Role Documentation.

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -7,10 +7,60 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
+/**
+ * @OA\Schema(
+ *   schema="Role",
+ *   title="Role",
+ *   description="The role model provides authorization to users.",
+ * )
+ */
 class Role extends Model
 {
     use HasFactory, HasUuids;
 
+    /**
+     * @OA\Property(
+     *    title="id",
+     *    description="Role id",
+     *    format="uuid",
+     *    example="123e4567-e89b-12d3-a456-426614174000"
+     * )
+     */
+    private $id;
+
+    /**
+     * @OA\Property(
+     *    title="name",
+     *    description="Role name",
+     *    type="string",
+     *    example="admin"
+     * )
+     */
+    private $name;
+
+    /**
+     * @OA\Property(
+     *    title="created_at",
+     *    description="Role created at",
+     *    example="2021-01-01 00:00:00"
+     * )
+     */
+    private $created_at;
+
+    /**
+     * @OA\Property(
+     *    title="updated_at",
+     *    description="Role updated at",
+     *    example="2021-01-01 00:00:00"
+     * )
+     */
+    private $updated_at;
+
+    /**
+     * Many-to-many relationship with users.
+     *
+     * @return BelongsToMany
+     */
     public function users(): BelongsToMany
     {
         return $this->belongsToMany(User::class);
