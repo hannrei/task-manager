8260a6a577afcee88a7a70b13ba1f7dcb9ae496f
hannrei <130257338+hannrei@users.noreply.github.com>
2023-09-13 21:37:31 +0200

    Installed darkaonline/l5-swagger.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,30 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    /**
+     * @OA\Info(
+     *      version="1.0.0",
+     *      title="Task Manager API Documentation",
+     *      description="Task Manager API Documentation. This is a sample API for Task Manager.",
+     *      @OA\Contact(
+     *          email="demo@email.com"
+     *      ),
+     *      @OA\License(
+     *          name="",
+     *          url=""
+     *      )
+     * )
+     *
+     * @OA\Server(
+     *      url=L5_SWAGGER_CONST_HOST,
+     *      description="Task Manager API Server"
+     * )
+
+     *
+     * @OA\Tag(
+     *     name="Task Manager",
+     *     description="API Endpoints of Task Manager"
+     * )
+     */
 }
