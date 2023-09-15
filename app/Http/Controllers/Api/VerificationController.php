f98742fa78e963a2f84fa60471a9fb83e9925ac1
hannrei <130257338+hannrei@users.noreply.github.com>
2023-09-15 12:33:02 +0200

    VerificationController Documentation.

--- app/Http/Controllers/Api/VerificationController.php
+++ app/Http/Controllers/Api/VerificationController.php
@@ -8,9 +8,40 @@ use Illuminate\Http\Request;
 
 class VerificationController extends Controller
 {
-    public function verify($user_id, Request $request) {
+    /**
+     * @OA\Get(
+     *     path="/api/email/verify/{id}",
+     *     summary="Verify email",
+     *     tags={"Email Verification"},
+     *     description="Verifies the user email if the email is not verified yet.",
+     *     operationId="verify",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="User id",
+     *         required=true,
+     *     ),
+     *     @OA\Response(
+     *         response=302,
+     *         description="Redirects to the API documentation page.",
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Invalid/Expired url provided.",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="Invalid/Expired url provided."
+     *             )
+     *         )
+     *     )
+     * )
+     */
+    public function verify($user_id, Request $request)
+    {
         if (!$request->hasValidSignature()) {
-            return response()->json(["msg" => "Invalid/Expired url provided."], 401);
+            return response()->json(["message" => "Invalid/Expired url provided."], 401);
         }
 
         $user = User::findOrFail($user_id);
@@ -22,13 +53,46 @@ class VerificationController extends Controller
         return redirect()->to('/api/documentation');
     }
 
-    public function resend() {
+    /**
+     * @OA\Get(
+     *     path="/api/email/resend",
+     *     summary="Resend verification email",
+     *     tags={"Email Verification"},
+     *     description="Resends the verification email if the email is not verified yet.",
+     *     operationId="resend",
+     *     @OA\Response(
+     *         response=200,
+     *         description="Email verification link sent to the user email address.",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="Email verification link sent to the user email address."
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Email already verified.",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="Email already verified."
+     *             )
+     *         )
+     *     )
+     * )
+     */
+
+    public function resend()
+    {
         if (auth()->user()->hasVerifiedEmail()) {
             return response()->json(["message" => "Email already verified."], 400);
         }
 
         auth()->user()->sendEmailVerificationNotification();
 
-        return response()->json(["message" => "Email verification link sent on your email id"]);
+        return response()->json(["message" => "Email verification link sent to your email address."], 200);
     }
 }
