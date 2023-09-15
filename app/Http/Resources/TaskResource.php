406207758a1a17b6c6d6aba61daf0654930e0de7
hannrei <130257338+hannrei@users.noreply.github.com>
2023-09-15 12:56:27 +0200

    TaskResource Documentation

--- app/Http/Resources/TaskResource.php
+++ app/Http/Resources/TaskResource.php
@@ -6,6 +6,24 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Storage;
 
+
+/**
+ * @OA\Schema(
+ *     schema="TaskResource",
+ *     type="object",
+ *     title="TaskResource",
+ *     @OA\Property(property="id", type="integer", example="1"),
+ *     @OA\Property(property="title", type="string", example="Task title"),
+ *     @OA\Property(property="description", type="string", example="Task description"),
+ *     @OA\Property(property="due_date", type="string", format="date", example="2021-01-01"),
+ *     @OA\Property(property="completed", type="boolean", example="false"),
+ *     @OA\Property(property="created_by", type="object", ref="#/components/schemas/UserResource"),
+ *     @OA\Property(property="assigned_to", type="object", ref="#/components/schemas/UserResource"),
+ *     @OA\Property(property="file", type="string", format="binary", example="http://localhost:8000/api/tasks/1/file"),
+ *     @OA\Property(property="created_at", type="string", format="date-time", example="2021-01-01T00:00:00.000000Z"),
+ *     @OA\Property(property="updated_at", type="string", format="date-time", example="2021-01-01T00:00:00.000000Z"),
+ * )
+ */
 class TaskResource extends JsonResource
 {
     /**
