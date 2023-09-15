ef875f2c09bff9b0cd18f5fdab210b75e9bbef8b
hannrei <130257338+hannrei@users.noreply.github.com>
2023-09-15 11:47:26 +0200

    Updated Role Documentation.

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -12,45 +12,39 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
  *   schema="Role",
  *   title="Role",
  *   description="The role model provides authorization to users.",
+ *   @OA\Property(
+ *     property="id",
+ *     title="id",
+ *     description="Role id",
+ *     format="uuid",
+ *     example="123e4567-e89b-12d3-a456-426614174000"
+ *   ),
+ *   @OA\Property(
+ *     property="name",
+ *     title="name",
+ *     description="Role name",
+ *     type="string",
+ *     example="admin"
+ *   ),
+ *   @OA\Property(
+ *     property="created_at",
+ *     title="created_at",
+ *     description="Role created at",
+ *     example="2021-01-01 00:00:00"
+ *   ),
+ *   @OA\Property(
+ *     property="updated_at",
+ *     title="updated_at",
+ *     description="Role updated at",
+ *     example="2021-01-01 00:00:00"
+ *   )
  * )
  */
+
 class Role extends Model
 {
     use HasFactory, HasUuids;
 
-    /**
-     * @OA\Property(
-     *    title="id",
-     *    description="Role id",
-     *    format="uuid",
-     *    example="123e4567-e89b-12d3-a456-426614174000"
-     * )
-     */
-
-    /**
-     * @OA\Property(
-     *    title="name",
-     *    description="Role name",
-     *    type="string",
-     *    example="admin"
-     * )
-     */
-
-    /**
-     * @OA\Property(
-     *    title="created_at",
-     *    description="Role created at",
-     *    example="2021-01-01 00:00:00"
-     * )
-     */
-
-    /**
-     * @OA\Property(
-     *    title="updated_at",
-     *    description="Role updated at",
-     *    example="2021-01-01 00:00:00"
-     * )
-     */
 
     /**
      * Many-to-many relationship with users.
