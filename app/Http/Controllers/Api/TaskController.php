b23aea02be34f90c693f8d7c2dc5cce30d378614
hannrei <130257338+hannrei@users.noreply.github.com>
2023-09-17 15:41:31 +0200

    TaskController Documentation.

--- app/Http/Controllers/Api/TaskController.php
+++ app/Http/Controllers/Api/TaskController.php
@@ -20,7 +20,56 @@ class TaskController extends Controller
     }
 
     /**
-     * Display a listing of the resource.
+     * @OA\Get(
+     *     path="/api/tasks",
+     *     summary="Get all tasks",
+     *     tags={"Tasks"},
+     *     description="Returns all tasks from the system that the user has access to.",
+     *     operationId="indexTasks",
+     *     security={{"bearerAuth":{}}},
+     *     @OA\Parameter(
+     *         name="filter",
+     *         in="query",
+     *         description="Filter tasks by status",
+     *         required=false,
+     *         @OA\Schema(
+     *             type="string",
+     *             enum={"completed", "incompleted", "overdue", "assigned_to_me", "created_by_me", "assigned_to_others", "created_by_others"}
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         name="sort",
+     *         in="query",
+     *         description="Sort tasks by field",
+     *         required=false,
+     *         @OA\Schema(
+     *             type="string",
+     *             enum={"dueDate", "-dueDate", "title", "-title", "completed", "-completed", "createdAt", "-createdAt"}
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Tasks retrieved successfully",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="data",
+     *                 type="array",
+     *                 @OA\Items(ref="#/components/schemas/TaskResource")
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Unauthenticated",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="Unauthenticated."
+     *             )
+     *         )
+     *    ),
+     * )
      */
     public function index(Request $request)
     {
@@ -76,7 +125,61 @@ class TaskController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
+     * @OA\Post(
+     *     path="/api/tasks",
+     *     summary="Create a task",
+     *     tags={"Tasks"},
+     *     description="Create a task",
+     *     operationId="storeTasks",
+     *     security={{"bearerAuth":{}}},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         description="Task object that needs to be created",
+     *         @OA\JsonContent(
+     *             required={"title"},
+     *             @OA\Property(property="title", type="string", example="Task title"),
+     *             @OA\Property(property="description", type="string", example="Task description"),
+     *             @OA\Property(property="assigned_to", type="string", example="9a136676-8ebb-4304-9de9-78bc74ab1d69", description="User ID or email"),
+     *             @OA\Property(property="due_date", type="string", format="date", example="2021-01-01"),
+     *             @OA\Property(property="file", type="string", format="binary", example="file.pdf"),
+     *             @OA\Property(property="completed", type="boolean", example="false"),
+     *             @OA\Property(property="created_by", type="string", example="9a136676-8ebb-4304-9de9-78bc74ab1d69"),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="Task created successfully",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="data",
+     *                 type="object",
+     *                 ref="#/components/schemas/TaskResource"
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Unauthenticated",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="Unauthenticated."
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=422,
+     *         description="Unprocessable Entity",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="The given data was invalid."
+     *             ),
+     *         ),
+     *     ),
+     * )
      */
     public function store(Request $request)
     {
@@ -101,7 +204,7 @@ class TaskController extends Controller
         ]);
 
         if ($request->has('file')) {
-            Storage::put('users/' . $assignedTo . '/'. 'tasks/' . $task->id . '.pdf', file_get_contents($request->file->getRealPath()));
+            Storage::put('users/' . $assignedTo . '/' . 'tasks/' . $task->id . '.pdf', file_get_contents($request->file->getRealPath()));
         }
 
         if ($assignedTo !== auth()->user()->id) {
@@ -110,8 +213,57 @@ class TaskController extends Controller
         return new TaskResource($task);
     }
 
+
     /**
-     * Display the specified resource.
+     * @OA\Get(
+     *     path="/api/tasks/{task}",
+     *     summary="Display the specified task",
+     *     description="Display the specified task",
+     *     operationId="showTasks",
+     *     tags={"Tasks"},
+     *     security={{"bearerAuth":{}}},
+     *     @OA\Parameter(
+     *         name="task",
+     *         in="path",
+     *         description="The ID of the task to be displayed",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="string",
+     *             format="uuid"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Task resource",
+     *         @OA\JsonContent(ref="#/components/schemas/TaskResource")
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Unauthenticated",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="Unauthenticated."
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=403,
+     *         description="Forbidden",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="Forbidden."
+     *             )
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Task not found"
+     *     )
+     * )
      */
     public function show(Task $task)
     {
@@ -119,15 +271,122 @@ class TaskController extends Controller
         return new TaskResource($task);
     }
 
+    /**
+     * Download the file attached to the given task.
+     *
+     * @OA\Get(
+     *     path="/api/tasks/{task}/file",
+     *     summary="Download the file attached to the given task",
+     *     description="Download the file attached to the given task",
+     *     operationId="downloadFileTasks",
+     *     tags={"Tasks"},
+     *     security={{"bearerAuth":{}}},
+     *     @OA\Parameter(
+     *         name="task",
+     *         in="path",
+     *         description="ID of the task to download the file from",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="string",
+     *             format="uuid"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="File downloaded successfully"
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Unauthorized"
+     *     ),
+     *     @OA\Response(
+     *         response=403,
+     *         description="Forbidden"
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Not Found"
+     *     )
+     * )
+     */
     public function downloadFile(Task $task)
     {
         $this->authorize('view', $task);
 
+        if($task->file === null) {
+            return response()->json([
+                'message' => 'No file attached to this task.'
+            ], 404);
+        }
         return Storage::download('users/' . $task->assignee->id . '/' . 'tasks/' . $task->id . '.pdf');
     }
 
+
     /**
-     * Update the specified resource in storage.
+     * @OA\Put(
+     *     path="/api/tasks/{task}",
+     *     summary="Update a task",
+     *     description="Update a task by ID",
+     *     operationId="updateTasks",
+     *     tags={"Tasks"},
+     *     security={{"bearerAuth":{}}},
+     *     @OA\Parameter(
+     *         name="task",
+     *         in="path",
+     *         description="ID of the task to update",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="string",
+     *             format="uuid"
+     *         )
+     *     ),
+     *     @OA\RequestBody(
+     *         @OA\JsonContent(
+     *             @OA\Property(property="title", type="string", example="Task title"),
+     *             @OA\Property(property="description", type="string", example="Task description"),
+     *             @OA\Property(property="assigned_to", type="string", example="9a136676-8ebb-4304-9de9-78bc74ab1d69", description="User ID or email"),
+     *             @OA\Property(property="due_date", type="string", format="date", example="2021-01-01"),
+     *             @OA\Property(property="file", type="string", format="binary", example="file.pdf"),
+     *             @OA\Property(property="completed", type="boolean", example="false"),
+     *             @OA\Property(property="created_by", type="string", example="9a136676-8ebb-4304-9de9-78bc74ab1d69"),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Task updated successfully",
+     *         @OA\JsonContent(ref="#/components/schemas/Task")
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Invalid input",
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Unauthenticated",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="Unauthenticated."
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=403,
+     *         description="Forbidden",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="Forbidden."
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Task not found",
+     *     ),
+     * )
      */
     public function update(Request $request, Task $task)
     {
@@ -141,13 +400,46 @@ class TaskController extends Controller
         ]);
 
         if ($request->has('file')) {
-            Storage::put('users/' . $task->assignee->id . '/'. 'tasks/' . $task->id . '.pdf', file_get_contents($request->file->getRealPath()));
+            Storage::put('users/' . $task->assignee->id . '/' . 'tasks/' . $task->id . '.pdf', file_get_contents($request->file->getRealPath()));
         }
         $task->update($validatedData);
 
         return new TaskResource($task);
     }
 
+    /**
+     * @OA\Put(
+     *     path="/api/tasks/{task}/complete",
+     *     summary="Mark a task as completed",
+     *     description="Mark a task as completed and notify the assignee if they are not the creator.",
+     *     operationId="completeTask",
+     *     security={{"bearerAuth":{}}},
+     *     tags={"Tasks"},
+     *     @OA\Parameter(
+     *         name="task",
+     *         in="path",
+     *         description="ID of the task to complete",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="string",
+     *             format="uuid"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Task resource",
+     *         @OA\JsonContent(ref="#/components/schemas/TaskResource")
+     *     ),
+     *     @OA\Response(
+     *         response=403,
+     *         description="Forbidden"
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Not Found"
+     *     )
+     * )
+     */
     public function complete(Task $task)
     {
         $this->authorize('complete', $task);
@@ -163,7 +455,57 @@ class TaskController extends Controller
     }
 
     /**
-     * Remove the specified resource from storage.
+     * @OA\Delete(
+     *     path="/api/tasks/{task}",
+     *     summary="Delete a task",
+     *     description="Delete a task by ID",
+     *     operationId="deleteTask",
+     *     security={{"bearerAuth":{}}},
+     *     tags={"Tasks"},
+     *     @OA\Parameter(
+     *         name="task",
+     *         in="path",
+     *         description="ID of the task to delete",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="string",
+     *             format="uuid"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Task deleted successfully",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="Task deleted successfully."
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=403,
+     *         description="Forbidden",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="Forbidden."
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Task not found",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="message",
+     *                 type="string",
+     *                 example="Task not found."
+     *             )
+     *         )
+     *     )
+     * )
      */
     public function destroy(Task $task)
     {
